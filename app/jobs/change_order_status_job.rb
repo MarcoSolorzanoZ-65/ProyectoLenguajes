3fed74f28dfcf483f98754f05ce21dc0000f0e60
The65Dude <marcobreda34@gmail.com>
2023-07-08 06:42:25 -0600

    Done

--- app/jobs/change_order_status_job.rb
+++ app/jobs/change_order_status_job.rb
@@ -2,7 +2,7 @@
 class ChangeOrderStatusJob < ApplicationJob
   queue_as :default
 
-  MAX_PERFORM_COUNT = 5
+  MAX_STATUS = 5
 
   def perform(order_id, perform_count = 1)
     order = Order.find(order_id)
@@ -11,11 +11,13 @@ class ChangeOrderStatusJob < ApplicationJob
     # Skip status 4 and go directly to 5 if the current status is not 4
     next_status = current_status == 4 ? current_status : current_status + 1
 
-    # Update the order status
-    order.update(order_status: next_status)
+    # Update the order status only if the new status is less than or equal to MAX_STATUS
+    if next_status <= MAX_STATUS
+      order.update(order_status: next_status)
+    end
 
-    # Re-enqueue the job to run again after 5 minutes if the new status is less than 5
-    if next_status <= MAX_PERFORM_COUNT
+    # Re-enqueue the job to run again after 5 minutes if the new status is less than 5 and perform_count is less than MAX_STATUS
+    if next_status < MAX_STATUS && perform_count < MAX_STATUS
       self.class.set(wait: 5.minutes).perform_later(order_id, perform_count + 1)
     end
   end
